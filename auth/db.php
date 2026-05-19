e8a09da192c9deed6ee4d21ea64c04e3eb952910
Hemanth Gavara <hemanthgavara9@gmail.com>
2026-05-19 10:40:44 +0530

    DB File has been configured

---
+++ auth/db.php
@@ -0,0 +1,14 @@
+<?php 
+
+require 'vendor/autoload.php';
+try{
+$client = new MongoDB/Client("mongodb://localhost:27017/");
+$db = $client -> spaceborn;
+}
+catch(Exception $e) {
+
+    echo 'Connection Failed : '. $e ;
+
+}
+
+?>
