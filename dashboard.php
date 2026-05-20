8e2f49e655e4bafabb64dc0b7f9a3aa224d6836f
Hemanth Gavara <hemanthgavara9@gmail.com>
2026-05-20 15:16:21 +0530

    Something is done

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,5 @@
 <?php 
-session_start();
-if(!isset($_SESSION['id']) || !isset($_SESSION['email'])){
-    header('Location: index.php');
-    exit;
-}
-
-$name = $_SESSION['name'];
-$plan = $_SESSION['user_sub']['plan_name'] ?? 'Free';
-
+require "auth/session_guard.php";
 
 ?>
 <!DOCTYPE html>
