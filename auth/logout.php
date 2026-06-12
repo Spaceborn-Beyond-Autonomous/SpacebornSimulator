7a3187803a202c315db42b1653795d866806df88
Poornachandran <poorna2006poorna@gmail.com>
2026-06-12 12:47:34 +0530

    security: add session_config, session_regenerate_id to logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,19 +1,24 @@
-<?php 
+<?php
 
-require 'db.php';
+require_once __DIR__ . '/session_config.php';
+sb_configure_session();
 session_start();
 
-if(!isset($_SESSION['id'])){
+if (!isset($_SESSION['id'])) {
     header('Location: ../index.php');
     exit;
 }
-$s = $db -> sessions -> updateOne(
+
+require_once __DIR__ . '/db.php';
+
+$db->sessions->updateOne(
     ['_id' => new MongoDB\BSON\ObjectId($_SESSION['id'])],
-    [ '$set' =>['is_running' => false]]);
+    ['$set' => ['is_running' => false]]
+);
 
 $_SESSION = [];
+session_regenerate_id(true);
 session_destroy();
+
 header('Location: ../index.php');
 exit;
-
-?>
